03da2ff560eedbd96bb226095d3d936792c06a03
etpinard <etienne.t.pinard@gmail.com>
2015-06-09 18:04:58 -0400

    make output bundle requireble

--- src/end.js
+++ src/end.js
@@ -1,1 +1,4 @@
-})();
+}
+
+module.exports = addProjectionToD3;
+
--- src/start.js
+++ src/start.js
@@ -1,1 +1,18 @@
-(function() {
+/*
+ * This file is auto-generated using:
+ *
+ * https://github.com/etpinard/d3-geo-projection
+ *
+ * a fork of
+ *
+ * https://github.com/d3/d3-geo-projection
+ *
+ * containing only the 'most useful' projection types.
+ *
+ * The output bundle is meant to be required in e.g. :
+ *
+ * var addProjectionToD3 = require('./d3.geo.projection')
+ *
+ */
+
+function addProjectionToD3() {
